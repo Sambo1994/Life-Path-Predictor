52a1d1f92a10d87f863aca171481379ae456823c
Sambo1994 <83902343+Sambo1994@users.noreply.github.com>
2025-02-20 18:52:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,10 +35,10 @@ function predictLife() {
 
 function getLifeExpectancy(birthYear, sex) {
     const baseLifeExpectancy = {
-        1900: { male: 70, female: 82 },
-        1950: { male: 73, female: 79 },
-        2000: { male: 73, female: 79 },
-        2025: { male: 70, female: 82 }
+        1900: { male: 74, female: 79 },
+        1950: { male: 71, female: 82 },
+        2000: { male: 74, female: 79 },
+        2025: { male: 71, female: 82 }
     };
 
     const years = Object.keys(baseLifeExpectancy).map(Number).sort((a, b) => a - b);
